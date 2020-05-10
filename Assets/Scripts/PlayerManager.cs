a2491ae4107d076f308361dec7e08ad0c321084f
tbrinkle <tylerbrinkley96@gmail.com>
2020-05-10 01:47:04 -0400

    Fixed Player Data read/write (not displayed properly yet)

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Newtonsoft.Json;
+using System.IO;
+
+
+public class Player {
+    public string playerName;
+    public int playerScore;
+   // public string date;
+}
+
 
 public class PlayerManager : MonoBehaviour
 {
 
     //public GameObject keyboard;
-    
     public TMP_InputField nameInputField;
     public string playerName;
     public int playerScore;
@@ -21,17 +30,11 @@ public class PlayerManager : MonoBehaviour
     void Start() {
         playerName = "Unknown";
         playerScore = 0;
-
         nameInputField.onValueChanged.AddListener(delegate {setPlayerName(); });
-    }
-
-    void Update()
-    {
-        
+        savePlayer();
     }
 
     public void setPlayerName() {
-        //keyboard.SetActive(true);
         playerName = nameInputField.text;
     }
 
@@ -47,4 +50,50 @@ public class PlayerManager : MonoBehaviour
         return playerScore;
     }
 
+     public void SaveFile(Player p)
+     {
+         //string location = Application.persistentDataPath + "/playerdata.dat";
+         string location = "Assets/Resources/playerdata.dat";
+         JsonSerializer serializer = new JsonSerializer();
+         List<Player> playerList = new List<Player>();
+
+
+         if (File.Exists(location)) {
+            using (StreamReader file = File.OpenText(location)) {
+                List<Player> playerListData = (List<Player>)serializer.Deserialize(file, typeof (List<Player>));
+                playerList = playerListData;
+            }
+            
+            playerList.Add(p);
+            var data = JsonConvert.SerializeObject(playerList);
+                           
+            using (StreamWriter sw = new StreamWriter(location))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                serializer.Serialize(writer, playerList);
+            }
+            
+         } else { 
+            FileStream file;
+            file = File.Create(location);
+            file.Close();
+            playerList.Add(p);
+            using (StreamWriter sw = new StreamWriter(location))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                serializer.Serialize(writer, playerList);
+            }
+         }       
+     }
+
+    public void savePlayer() {
+        Player savePlayer = new Player();
+        savePlayer.playerName = getPlayerName();
+        savePlayer.playerScore = getPlayerScore();
+        SaveFile(savePlayer);
+    }
+
 }
+
+
+
---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Newtonsoft.Json;
+using System.IO;
+
+
+public class ScoreBoard : MonoBehaviour
+{
+    public GameObject scoreContainer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        loadScoreBoard();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void loadScoreBoard() {
+        List<Player> playerData = new List<Player>();
+        //string location = Application.persistentDataPath + "/playerdata.dat";
+        string location = "Assets/Resources/playerdata.dat";
+
+        JsonSerializer serializer = new JsonSerializer();
+        List<Player> playerList = new List<Player>();
+
+        if (File.Exists(location)) {
+            using (StreamReader file = File.OpenText(location)) {
+                List<Player> playerListData = (List<Player>)serializer.Deserialize(file, typeof (List<Player>));
+                playerList = playerListData;
+            }
+
+            for (int i = 0; i < playerList.Count; i++)
+            {
+                string playerName = playerList[i].playerName;
+                int playerScore = playerList[i].playerScore;
+
+                GameObject boardName = new GameObject("child");
+                GameObject boardScore = new GameObject("child");
+                boardName.transform.SetParent(this.transform);
+                boardScore.transform.SetParent(this.transform);
+
+                boardName .AddComponent<Text>().text = playerName;
+                boardScore .AddComponent<Text>().text = playerScore.ToString();
+            }
+
+
+            
+         } else { 
+                // No Data to add!
+                // Do something with the empty space . . .
+         }       
+    }
+}
